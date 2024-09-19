49f412d405e6983cfedaee1d22e189f29711e93c
Ananda Dwi Mirza <ananda.mirza@arkamaya.co.id>
2024-09-19 08:32:38 +0700

    setup mongoose db

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,20 @@
-const http = require('http')
-const app = require(`./app`)
+const http = require('http');
+const app = require(`./app`);
+const mongoose = require('mongoose');
+
 const {loadPlanetsData} = require('./models/planets.model')
 const PORT = process.env.PORT || 8000;
+
+const MONGO_URL = 'mongodb+srv://Dev:nanda1@nasacluster.iorhfai.mongodb.net/nasa?retryWrites=true&w=majority&appName=NasaCluster'
+
 const server = http.createServer(app)
 
+// mongoose.connection.once('open', () => {
+//     console.log('server is ready')
+// })
+
 async function startServer(){
+await mongoose.connect(MONGO_URL)
 await loadPlanetsData()
 
 server.listen(PORT, () => {
